30e8f5e2cb9edeca3e6cbac4bf6dfa0ed0a16311
Prajakta Nitin Mane <mane@lab1-1.eng.utah.edu>
2015-01-17 10:53:38 -0700

    Message for extraction

--- main.c
+++ main.c
@@ -4,5 +4,18 @@ int main(void)
 {
         printFunc();
 	printf("Hello\n");
+        unsigned int arg = 0x01;
+        unsigned int arg2 = 0x02;
+        int compare = arg < arg2;
+        printf("Compare=%d\n",compare);
+        unsigned int arg3 = 0x0201;
+        int length =0;
+        unsigned int extract=0;
+    
+        for(length = 0; length < 2; length++)
+        {
+            extract = ((arg3 >> (8*length)) & 0xFF);
+            printf("%d\n",extract);
+        }
         return 0;
 }
